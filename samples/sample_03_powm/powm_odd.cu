839fb896635ac30ae336310f64a76b317d9b7dbd
pradeepben3 <97969637+pradeepben3@users.noreply.github.com>
2023-01-12 00:06:07 +0530

    Update powm_odd.cu

--- samples/sample_03_powm/powm_odd.cu
+++ samples/sample_03_powm/powm_odd.cu
@@ -1,26 +1,3 @@
-/***
-
-Copyright (c) 2018-2019, NVIDIA CORPORATION.  All rights reserved.
-
-Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the "Software"),
-to deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-IN THE SOFTWARE.
-
-***/
 
 #include <stdio.h>
 #include <stdint.h>
@@ -253,8 +230,8 @@ class powm_odd_t {
   
     for(index=0;index<count;index++) {
       random_words_base(instances[index].x._limbs, params::BITS/32);
-      random_words_power(instances[index].power._limbs, params::BITS/32);
-      random_words_exp(instances[index].modulus._limbs, params::BITS/32);
+      random_words_power(instances[index].power._limbs, params::(32*index)+32);
+      random_words_exp(instances[index].modulus._limbs, params::(32*index)+32);
 
       // ensure modulus is odd
       instances[index].modulus._limbs[0] |= 1;
@@ -384,7 +361,7 @@ int run_test(uint32_t instance_count) {
 
 int fun() {
   typedef powm_params_t<8, 1024, 5> params;
-  return run_test<params>(100000);
+  return run_test<params>(3000);
 }
 int main(int num_numbers, uint32_t *num_base,uint32_t *num_power,uint32_t *num_exp ){
  int i;
@@ -398,5 +375,5 @@ int main(int num_numbers, uint32_t *num_base,uint32_t *num_power,uint32_t *num_e
         expo[i]= num_exp[i];
     }
     fun();
-    return expo[6000];
+    return 0;
  }
